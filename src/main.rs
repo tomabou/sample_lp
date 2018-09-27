5c12f42fbe816cabec434039cb3483e3b0645729
tomabou <kazu.8128.9.6@gmail.com>
2018-09-27 11:12:33 +0900

    add simple

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+    println!("Start LP");
+
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("しっぱい");
+    println!("{}",guess);
 }
