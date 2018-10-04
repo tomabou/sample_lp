3ae727a3471832cfd77debf848158f0a362ff692
tomabou <kazu.8128.9.6@gmail.com>
2018-10-04 12:08:06 +0900

    ratio_test

--- src/main.rs
+++ src/main.rs
@@ -17,22 +17,28 @@ struct Dict{
     max: f64,
 }
 
-fn argmax<T: PartialOrd>(v: &Vec<T>) -> usize{
+fn argmax(v: &Vec<f64>) -> Option<usize>{
     assert!(v.len() >0);
-    let mut max = &v[0];
+    let mut max = v[0];
     let mut index = 0;
     for (i,x) in v.into_iter().enumerate() {
-        max = if *max > *x {max} else {index = i; x};
+        max = if max > *x {max} else {index = i; *x};
     }
-    index
+    if max > 0.0 {Some(index)} else {None}
 }
 
 #[test]
 fn test_argmax(){
     let v: Vec<f64> = vec![2.0,3.0,1.0];
-    let i = argmax(&v);
+    let i = argmax(&v).unwrap();
     assert_eq!(i,1);
+    let v: Vec<f64> = vec![-2.0,-3.0,-1.0];
+    let i = argmax(&v);
+    assert_eq!(i,None);
 }
+
+
+use std::f64;
 
 impl Dict{
     fn from_canonical (can: &Canonical) -> Dict{
@@ -48,10 +54,31 @@ impl Dict{
         }
     }
     pub fn solve(&mut self){
-
+        let i = self.choose_column().unwrap();
+        self.pivot(i);
     }
-    fn choose_column(&self) -> usize{
+    fn choose_column(&self) -> Option<usize>{
         argmax(&self.c)
+    }
+    fn pivot(&mut self,i: usize) -> Option<()>{ 
+        let j = self.ratio_test(i)?;
+
+        Some(())
+    }
+    fn ratio_test(&self, i: usize) -> Option<usize>{
+        let mut index = 0;
+        let mut min =  f64::INFINITY;
+        for j in 0..self.base.len(){
+            if self.b[j] < 0.0 {
+                continue;
+            }
+            let ratio = self.b[j] / self.a[i][j];
+            min = if ratio > min {
+                index = i;
+                ratio
+            } else {min};
+        }        
+        if min == f64::INFINITY {None} else {Some(index)}
     }
 }
 
